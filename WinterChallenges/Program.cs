675b82a7b24c5bed57bc3a92d25fbf98b0ac9d6b
Nathan Pabst <nathan.pabst@gmail.com>
2018-12-24 14:53:04 -0600

    wip

--- WinterChallenges/Program.cs
+++ WinterChallenges/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace WinterChallenges
 {
@@ -6,7 +7,18 @@ namespace WinterChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Palindrome Checker.");
+            Console.WriteLine("Enter a word or phrase to begin.");
+            Console.ReadLine();
+        }
+
+        //Write a method that determines whether a phrase represents a palindrome.
+        //ignore the case, spaces and punctuation from the input.
+        //use system.linq 
+         
+        private static void TestForPalindrome(string input)
+        {
+            //var prepInput = String.Join("", input.ToList().Where(char => !char.IsPunctuation(char))).Replace(" ", "").ToLower();
         }
     }
 }
