9ca73b4e89477c1d64afa4d8564a2afed7883da6
Nathan Pabst <nathan.pabst@gmail.com>
2018-12-29 14:29:24 -0600

    top 20 combinations printing. refactored and removed unnecessary code.

--- WinterChallenges/Program.cs
+++ WinterChallenges/Program.cs
@@ -13,15 +13,6 @@ namespace WinterChallenges
         {
             string path = @"c:\Users\natha\source\repos\WinterChallenges\WinterChallenges\PizzaData\Pizza.json";
 
-            //if (!File.Exists(path))
-            //{
-            //    string createText = "Welcome to Pizza Tracker" + Environment.NewLine;
-            //    File.WriteAllText(path, createText);
-            //}
-
-            //string appendText = "Extra stuff" + Environment.NewLine;
-            //File.AppendAllText(path, appendText);
-
             string readPizzaData = File.ReadAllText(path);
             var pizzas = JsonConvert.DeserializeObject<List<PizzaConfigs>>(readPizzaData);
             var combinations = new List<PizzaConfigs>();
@@ -36,24 +27,17 @@ namespace WinterChallenges
                 {
                     pizza.count++;
                     combinations.Add(pizza);
+
                 }
             }
 
-            foreach (var combination in combinations.OrderBy(c => c.count).Reverse().Take(20))
+            foreach (var combination in combinations.OrderByDescending(c => c.count).Take(20)) 
             {
-                foreach (var pizza in combinations)
-                {
-                    Console.WriteLine($"{String.Join(" and ", pizza.toppings)} ordered {pizza.count} times");
-                }
+               Console.WriteLine($"{String.Join(" and ", combination.toppings)} || {combination.count} orders");
             }
 
+            //suspect that not all combinations are being represented 
 
-
-
-
-            Console.WriteLine(combinations);
-
-            
 
             //*************PALINDROMES****************************//
             //Console.WriteLine("Welcome to the Palindrome Checker.");
